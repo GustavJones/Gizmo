60c2970ab277b7706e901670c2eeae6fa621379a
GustavJones <gustavjones3@gmail.com>
2023-11-02 10:40:36 +0200

    Linux Setup

--- include/Gizmo/Core.hpp
+++ include/Gizmo/Core.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#ifdef GIZMO_LINUX
+    #define GIZMO_API __attribute__((visibility("default")))
+#endif
+
 #ifdef GIZMO_WINDOWS
     #define GIZMO_API __declspec(dllexport)
-#elif GIZMO_LINUX
-    #define GIZMO_API __attribute__((visibility ("default")))
 #endif
